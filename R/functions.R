6db7cf62ccf022f33e5b7725cb08cacbbe7e4e0e
Karen Lykkebo <karenlykkebo@gmail.com>
2019-06-19 18:46:20 +0200

    more data changes

--- R/functions.R
+++ R/functions.R
@@ -40,7 +40,7 @@ require(Hmisc)
 
 # Save data
 # Orchids
-orchids1 <- fread("./data/orchidsgotland.csv", header = T, sep = ";")
+orchids1 <- fread(file ="./data/orchidsgotland.csv", header = T, sep = ";")
 # clean up dataframe
 got_orchids <- orchids1 %>%
   rename("lat" = "decimalLatitude",
@@ -48,10 +48,10 @@ got_orchids <- orchids1 %>%
   select(-c(1:3, 11:14, 16, 19:24, 29:45))
 
 # Rain
-got_rain <- fread("./data/rain_gotland.csv",header = T, sep = ";")
+got_rain <- fread(file = "./data/rain_gotland.csv",header = T, sep = ";")
 
 # Shapefile
-gotland <- st_read("./data/gotland.shp")
+gotland <- st_read(file = "./data/gotland.shp")
 
 # Save to package
 usethis::use_data(got_orchids, overwrite = T)
